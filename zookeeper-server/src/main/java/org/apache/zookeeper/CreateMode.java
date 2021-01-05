919baf0d8d3cd0bca39908264fc161ea2bf83a7a
qingmei <qingmei@fanhaoyue.com>
2021-01-05 17:03:44 +0800

    节点类型

--- zookeeper-server/src/main/java/org/apache/zookeeper/CreateMode.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/CreateMode.java
@@ -31,20 +31,24 @@ public enum CreateMode {
     /**
      * The znode will not be automatically deleted upon client's disconnect.
      */
+    // 持久节点
     PERSISTENT(0, false, false, false, false),
     /**
      * The znode will not be automatically deleted upon client's disconnect,
      * and its name will be appended with a monotonically increasing number.
      */
+    // 持久顺序节点
     PERSISTENT_SEQUENTIAL(2, false, true, false, false),
     /**
      * The znode will be deleted upon the client's disconnect.
      */
+    // 临时节点
     EPHEMERAL(1, true, false, false, false),
     /**
      * The znode will be deleted upon the client's disconnect, and its name
      * will be appended with a monotonically increasing number.
      */
+    // 临时有序节点
     EPHEMERAL_SEQUENTIAL(3, true, true, false, false),
     /**
      * The znode will be a container node. Container
